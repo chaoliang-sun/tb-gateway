1a0718f6ffeeea330faf42d1b3cdeec063234cca
weigs3 <gongsheng.wei@kuka.com>
2021-01-22 13:26:29 +0800

    修改镜像脚本-通过环境变量默认值 修改配置

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 if [[ $TB_HOST ]]; then
-	sed   's/\$\{TB_HOST\}/'"$TB_HOST"'/g' "$configs"/tb_gateway.yaml
+	sed   's/${TB_HOST}/'"$TB_HOST"'/g' "$configs"/tb_gateway.yaml
 fi
 if [[ $TB_PORT ]]; then
-	sed   's/\$\{TB_HOST\}/'"$TB_PORT"'/g' "$configs"/tb_gateway.yaml
+	sed   's/${TB_HOST}/'"$TB_PORT"'/g' "$configs"/tb_gateway.yaml
 fi
-sed  's/\$\{TB_CREDENTIALS\}/'"$TB_CREDENTIALS"'/g' "$configs"/tb_gateway.yaml
-sed   's/\$\{GATEWAY_TOKEN\}/'"$GATEWAY_TOKEN"'/g' "$configs"/tb_gateway.yaml
-sed   's/\$\{CONFIG_FILE\}/'"$CONFIG_FILE"'/g' "$configs"/tb_gateway.yaml
+sed  's/${TB_CREDENTIALS}/'"$TB_CREDENTIALS"'/g' "$configs"/tb_gateway.yaml
+sed   's/${GATEWAY_TOKEN\}/'"$GATEWAY_TOKEN"'/g' "$configs"/tb_gateway.yaml
+sed   's/${CONFIG_FILE\}/'"$CONFIG_FILE"'/g' "$configs"/tb_gateway.yaml
 
 #change mqtt.file
 cat "$configs"/tb_gateway.yaml
-sed  's/\$\{CB_HOST\}/'"CB_HOST"'/g' "$configs/$CONFIG_FILE"
-sed  's/\$\{CB_PORT\}/'"CB_PORT"'/g' "$configs/$CONFIG_FILE"
-sed  's/\$\{GATEWAY_CLIENT_ID\}/'"$GATEWAY_CLIENT_ID"'/g' "$configs/$CONFIG_FILE"
+sed  's/${CB_HOST}/'"CB_HOST"'/g' "$configs/$CONFIG_FILE"
+sed  's/${CB_PORT}/'"CB_PORT"'/g' "$configs/$CONFIG_FILE"
+sed  's/${GATEWAY_CLIENT_ID}/'"$GATEWAY_CLIENT_ID"'/g' "$configs/$CONFIG_FILE"
 sed  's/\$\{CA_PATH\}/'"$CA_PATH"'/g' "$configs/$CONFIG_FILE"
-sed  's/\$\{CB_USERNAME\}/'"$CB_USERNAME"'/g' "$configs/$CONFIG_FILE"
-sed  's/\$\{CB_PASSWORD\}/'"$CB_PASSWORD"'/g' "$configs/$CONFIG_FILE"
-sed  's/\$\{TOPIC_PREFIX\}/'"$TOPIC_PREFIX"'/g' "$configs/$CONFIG_FILE"
-sed  's/\$\{DEVICE_PATTEN\}/'"$DEVICE_PATTEN"'/g' "$configs/$CONFIG_FILE"
+sed  's/${CB_USERNAME}/'"$CB_USERNAME"'/g' "$configs/$CONFIG_FILE"
+sed  's/${CB_PASSWORD}/'"$CB_PASSWORD"'/g' "$configs/$CONFIG_FILE"
+sed  's/${TOPIC_PREFIX}/'"$TOPIC_PREFIX"'/g' "$configs/$CONFIG_FILE"
+sed  's/${DEVICE_PATTEN}/'"$DEVICE_PATTEN"'/g' "$configs/$CONFIG_FILE"
 
 cat "$configs/$CONFIG_FILE"
 
