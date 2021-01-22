ef2266a0d897796eeb54baab038d9c2023059a60
weigs3 <gongsheng.wei@kuka.com>
2021-01-22 14:24:37 +0800

    修改镜像脚本-通过环境变量默认值 修改配置

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,26 +4,26 @@ echo show gateway config -------------------------------------
 echo will change "$configs"/tb_gateway.yaml
 echo env are: $TB_HOST  $TB_PORT $TB_CREDENTIALS $GATEWAY_TOKEN   $CONFIG_FILE
 
-sed   's/${TB_HOST}/'"$TB_HOST"'/g' "$configs"/tb_gateway.yaml
+sed  -i 's/${TB_HOST}/'"$TB_HOST"'/g' "$configs"/tb_gateway.yaml
 
-sed   's/${TB_PORT}/'"$TB_PORT"'/g' "$configs"/tb_gateway.yaml
+sed  -i 's/${TB_PORT}/'"$TB_PORT"'/g' "$configs"/tb_gateway.yaml
 
-sed  's/${TB_CREDENTIALS}/'"$TB_CREDENTIALS"'/g' "$configs"/tb_gateway.yaml
-sed   's/${GATEWAY_TOKEN\}/'"$GATEWAY_TOKEN"'/g' "$configs"/tb_gateway.yaml
-sed   's/${CONFIG_FILE\}/'"$CONFIG_FILE"'/g' "$configs"/tb_gateway.yaml
+sed  -i 's/${TB_CREDENTIALS}/'"$TB_CREDENTIALS"'/g' "$configs"/tb_gateway.yaml
+sed   -i 's/${GATEWAY_TOKEN\}/'"$GATEWAY_TOKEN"'/g' "$configs"/tb_gateway.yaml
+sed  -i 's/${CONFIG_FILE\}/'"$CONFIG_FILE"'/g' "$configs"/tb_gateway.yaml
 
 #change mqtt.file
 echo show mqtt config -------------------------------------
 echo $CB_HOST $CB_PORT $CA_PATH $CB_USERNAME $CB_PASSWORD
 
-sed  's/${CB_HOST}/'"$CB_HOST"'/g' "$configs/$CONFIG_FILE"
-sed  's/${CB_PORT}/'"$CB_PORT"'/g' "$configs/$CONFIG_FILE"
-sed  's/${GATEWAY_CLIENT_ID}/'"$GATEWAY_CLIENT_ID"'/g' "$configs/$CONFIG_FILE"
-sed  's/${CA_PATH\}/'"$CA_PATH"'/g' "$configs/$CONFIG_FILE"
-sed  's/${CB_USERNAME}/'"$CB_USERNAME"'/g' "$configs/$CONFIG_FILE"
-sed  's/${CB_PASSWORD}/'"$CB_PASSWORD"'/g' "$configs/$CONFIG_FILE"
-sed  's/${TOPIC_PREFIX}/'"$TOPIC_PREFIX"'/g' "$configs/$CONFIG_FILE"
-sed  's/${DEVICE_PATTEN}/'"$DEVICE_PATTEN"'/g' "$configs/$CONFIG_FILE"
+sed -i 's/${CB_HOST}/'"$CB_HOST"'/g' "$configs/$CONFIG_FILE"
+sed -i 's/${CB_PORT}/'"$CB_PORT"'/g' "$configs/$CONFIG_FILE"
+sed -i 's/${GATEWAY_CLIENT_ID}/'"$GATEWAY_CLIENT_ID"'/g' "$configs/$CONFIG_FILE"
+sed -i 's/${CA_PATH\}/'"$CA_PATH"'/g' "$configs/$CONFIG_FILE"
+sed -i 's/${CB_USERNAME}/'"$CB_USERNAME"'/g' "$configs/$CONFIG_FILE"
+sed -i 's/${CB_PASSWORD}/'"$CB_PASSWORD"'/g' "$configs/$CONFIG_FILE"
+sed -i 's/${TOPIC_PREFIX}/'"$TOPIC_PREFIX"'/g' "$configs/$CONFIG_FILE"
+sed -i 's/${DEVICE_PATTEN}/'"$DEVICE_PATTEN"'/g' "$configs/$CONFIG_FILE"
 
 
 
